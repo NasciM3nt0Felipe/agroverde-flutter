458dee1e6188e8450414fb3d5802c2294f1d5643
NasciM3nt0Felipe <felipe-donascimento@hotmail.com>
2026-06-06 18:29:50 -0300

    feat: adiciona configuracao inicial do SQLite

---
+++ agroverde/lib/database/database_helper.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Database? _database;
+
+  static Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    }
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  static Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'agroverde.db');
+    debugPrint('Banco criado em: $path');
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  static Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT NOT NULL,
+        senha TEXT NOT NULL
+      )
+    ''');
+  }
+}
